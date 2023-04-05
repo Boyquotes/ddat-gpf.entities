1a884783395ba1228d759b978f6342e00ad8097b
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2023-04-05 12:20:32 +0100

    testenv changes
    improved debug labeling in entab_input
    integration with input_held signal in entab_input
    test window sizing

--- _testenv/test_entity_ability/entab_input/test_entab_input.gd
+++ _testenv/test_entity_ability/entab_input/test_entab_input.gd
@@ -1,18 +1,47 @@
 extends Node2D
 
+##############################################################################
+
+# testenv for activation controller
 
 ##############################################################################
 
-# TEST OUTCOMES
-# input activated has a minimum cooldown/delay it seems < SOLVED*
-# *relates to activation cap per frame, default should probably be higher
-# input toggled works
-# input confirmed press needs to unset after successful activate < FIXED
-# input confirmed hold works
-
+var debug_string_1_key = "ability uses"
+var debug_string_1_value = ""
+var debug_string_2_key = "confirmed press hold%"
+var debug_string_2_value = "0.0"
 
 var ability_activation_count = 0
-func _on_EntityAbilityController_activate_ability():
+
+onready var debug_label = $DebugLabel
+
+#############################################################################
+
+
+func _ready():
+	update_debug_label()
+
+
+#############################################################################
+
+
+func update_debug_label():
+	debug_label.text =\
+			debug_string_1_key+": "+debug_string_1_value+"\n"+\
+			debug_string_2_key+": "+debug_string_2_value+"\n"#+\
+
+
+#############################################################################
+
+
+func _on_ActivationController_activate_ability():
 	ability_activation_count += 1
 	print("ability activated, count: {x}".format({"x": ability_activation_count}))
+	debug_string_1_value = str(ability_activation_count)
+	update_debug_label()
+
+
+func _on_ActivationController_input_held(hold_remaining):
+	debug_string_2_value = str(hold_remaining)
+	update_debug_label()
 
