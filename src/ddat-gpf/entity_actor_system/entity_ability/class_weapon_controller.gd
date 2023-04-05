e9a881426c721f2b30e7c431baaf2ee27bb3ee74
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2023-04-05 11:07:33 +0100

    brainstorming update

--- src/ddat-gpf/entity_actor_system/entity_ability/class_weapon_controller.gd
+++ src/ddat-gpf/entity_actor_system/entity_ability/class_weapon_controller.gd
@@ -4,49 +4,56 @@ class_name WeaponController
 
 ##############################################################################
 
-# docstring
-
 # BRAINSTORMING
+#Revising for 23-04-05
 #
-#AbilityController is a node that spawns entities (entityArea, entityProjectile)
-#and acquires targets.
-#Previously named 'WeaponController'
-#
-#Key properties of the weapon controller are:
-#	FiringArc
-#		How much it can rotate (0 for fixed)
-#	SpawnPattern
-#		How many entities to spawn
-#		Where to spawn them relative to muzzle pos2D
-#		What entityMovement arguments to give them
+# [EntityAbilityTargeter]
+# FiringArc (max rotate toward target, 0 for fixed) should go to EntityAbilityTargeter
+# Targeter should have a 'allowed_to_rotate_toward' and rotate-toward-speed property
+# Targeter 'rotation' should be an invisible line
+# MOVE TO EntityAbilityTargeter:
+#	TargetGuiding Line:
+#		Visible line2d, for player guidance/optional arg for target-mouselook
+#	TargetAcquisition:
+#		Invisible line that checks if path would collide with ship
+#		(for auto target modes to not fire if would fire through ship)
+
+# [EntitySpawner]
+# SpawnPattern (how many to spawn) is going to EntitySpawner
+# SpawnPattern (spawn offset) is part of EntitySpawner
+# SpawnPatterns can push properties to entities, influencing spawned
+# entity behaviour (like lifespan/collision/movement)
+
+# [EntityArea]
+# SpawnPattern (movement patterns/args) should be part of the Entity itself
+#	(maybe with optional properties pushed by spawner)
+#	EntityMovementControllers can stack and apply multiple movement instructions per frame?
+#	Entities contain options for different lifespan/collison/movement packages?
+
+# [WeaponController/AbilityController]
+# previously pegged as part of weapon controller, should these be moved to abilityController parent?
+# Should these 
 #	AbilityWarmup
 #		How long it takes to fire when target found or input pressed
-#		optional arg for whether tracking during warmup
+#		optional arg for whether tracking during warmup (signal to targeter)
+#		need settings 
 #	AbilityCooldown
 #		how long before it can seek target or acknowledge input again
 #	(scope?)
 #	AbilityUses
 #		How many times it can spawn total, negative for infinite
+#		need signal update when spawning, reaching max spawns
 #	AbilityRefresh
 #		how long it takes to replenish spawn uses, nil or negative for never
+#		need signal update when replenishing
 #	RefreshAmount
 #		how many uses are refreshed on refresh timeout, negative for all
+#		need signal update when refreshed
 #	RefreshMode
 #		refresh-when-not-in-use, refresh-always, refresh-when-uses-spent
 #	CooldownMode
 #		as above (refreshMode)
 #
-#Key subnodes of the weapon controller are:
-#	TargetGuiding Line:
-#		Visible line2d, for player guidance/optional arg for target-mouselook
-#	TargetReticule
-#		Shown when off cooldown and trying to target
-#		need INPUT_CONFIRMED_PRESS or INPUT_CONFIRMED_HOLD
-#	TargetAcquisition:
-#		Invisible line that checks if path would collide with ship
-#		(for auto target modes to not fire if would fire through ship)
-#
-
 
 ##############################################################################
 
