73e28703c9e4711b0ad5a4bf6d7d7fc53e36db07
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2023-04-12 17:54:06 +0100

    property rename
    for readability

--- src/ddat-gpf/entity_systems/classes/class_object_pool.gd
+++ src/ddat-gpf/entity_systems/classes/class_object_pool.gd
@@ -36,10 +36,10 @@ var object_register = {}
 # these properties are the default values to assign to a new object
 # (this overrides object class defaults)
 # if a property isn't found here it will be set to the object's class default
-var forced_properties_on_init = {}
+var set_on_init = {}
 
-# the following properties function as the 'forced_properties_on_init'
-# property, but override only when the object is registered as 'active' or
+# the following properties function as the 'set_on_init' property except
+# they override only when the object is registered as 'active' or
 # 'inactive' by its corresponding objectPool.
 # devnote: take care in assigning frequently used properties here, as these
 #	properties will be assigned *every* time that the objectPool registers
@@ -47,9 +47,9 @@ var forced_properties_on_init = {}
 #	As such this property is best used for per-class flags to indicate
 #	it can do things again.
 # overrides for when registered as 'active' by the objectPool
-var forced_properties_on_active = {}
+var set_on_active = {}
 # overrides for when registered as 'inactive' by the objectPool
-var forced_properties_on_inactive = {}
+var set_on_inactive = {}
 
 ##############################################################################
 
@@ -63,13 +63,13 @@ var forced_properties_on_inactive = {}
 #	(see 'target_scene' for more detail)
 # #2, arg_forced_properties is a dictionary register of properties you wish to
 #	automatically set on any 
-#	(see 'forced_properties_on_init' for more detail)
+#	(see 'set_on_init' for more detail)
 # #3, arg_forced_on_active is as the above except occurs when an object is
 #	registered as 'active' by the objectPool
-#	(see 'forced_properties_on_active' for more detail)
+#	(see 'set_on_active' for more detail)
 # #4, arg_forced_on_inactive is as the above except occurs when an object is
 #	registered as 'inactive' by the objectPool
-#	(see 'forced_properties_on_inactive' for more detail)
+#	(see 'set_on_inactive' for more detail)
 func _init(
 		arg_object_scene: PackedScene,
 		arg_forced_properties: Dictionary = {},
@@ -77,9 +77,9 @@ func _init(
 		arg_forced_on_inactive: Dictionary = {}
 		):
 	self.target_scene = arg_object_scene
-	self.forced_properties_on_init = arg_forced_properties
-	self.forced_properties_on_active = arg_forced_on_active
-	self.forced_properties_on_inactive = arg_forced_on_inactive
+	self.set_on_init = arg_forced_properties
+	self.set_on_active = arg_forced_on_active
+	self.set_on_inactive = arg_forced_on_inactive
 
 
 # Called when the node enters the scene tree for the first time.
